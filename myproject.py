a26268356f09181a065d8a9c7788b7f04e456de2
tommy <tommy@admindeMacBook-Air.local>
2020-08-07 11:02:56 +0800

    flask app

--- myproject.py
+++ myproject.py
@@ -40,9 +40,9 @@ def login():
 
     return render_template('login.html')
 
-@app.route('/hello/<username>')
-def hello(username):
-    return render_template('hello.html', username=username)
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
      
 if __name__ == "__main__":
     app.debug = True
